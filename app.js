53b147c77da8db817c782e8ef02436188c40a124
Winterstorm <thunpwnz@gmail.com>
2019-03-12 21:22:11 +0400

    Mongoose connection

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const app = express()
+
+mongoose.connect('mongodb://localhost/formula-total')
+
+const db = mongoose.connection
+
+db.once('open',() => console.log('Server connected to the database'))
+
+const userSchema = new mongoose.Schema({
+    userName : String,
+    email:String,
+    password:String,
+    userBalance:Number,
+    userBets : [{
+        race:String,
+        win : Boolean
+    }]
+})
+
+const User = new mongoose.model('User',userSchema)
+
+const TestUser = new User({
+    userName:"TestUser",
+    email:"test@example.com",
+    password:"password",
+    userBalance:10000,
+    userBets :[ {
+        race:'Australia',
+        win:false
+    }]
+})
+
+TestUser.save((error,user) => {
+    if(error){
+        console.log(error)
+    } else {
+        console.log(user)
+    }
+
+})
+
+User.find({}).then(data => console.log(data))
+app.get('/',(request,response) => {
+    response.send('Hello world')
+})
+
+
+
+app.listen(3000,() => console.log(`Server launched at port 3000`))
